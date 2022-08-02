8857acd369796fcdab8e3c60ee44fcd01fbc3ea8
satyajit.kuila <satyajit.kuila@skuila.com>
2022-08-02 18:55:41 +0530

    Abstraction tutorial

---
+++ src/com/skuila/test27/Shapes.java
@@ -0,0 +1,11 @@
+package com.skuila.test27;
+
+public abstract class Shapes {
+	
+	public abstract void drawShape();
+
+	public abstract void colorShape();
+
+	public abstract void moveShape();
+
+}
